0cc3579becc5731d1b552dce6a34fd735cc67ac6
orjuelalaley20 <sebasorjuela@gmail.com>
2022-04-05 22:10:41 -0500

    style

--- codes/Subscriber.c
+++ codes/Subscriber.c
@@ -12,52 +12,58 @@
 int fd1;
 char mensaje[TAMMENSAJE];
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-  int  fd, nombre, creado = 0, res;
-  news datos;    
-  mode_t fifo_mode = S_IRUSR | S_IWUSR;
- 
-  // Se abre el pipe cuyo nombre se recibe como argumento del main. 
-  do { 
-     fd = open(argv[1], O_WRONLY);
-     if (fd == -1) {
+   int fd, nombre, creado = 0, res;
+   news datos;
+   mode_t fifo_mode = S_IRUSR | S_IWUSR;
+
+   // Se abre el pipe cuyo nombre se recibe como argumento del main.
+   do
+   {
+      fd = open(argv[1], O_WRONLY);
+      if (fd == -1)
+      {
          perror("pipe");
          printf(" Se volvera a intentar despues\n");
-	 sleep(10);        
-     } else creado = 1;
-  } while (creado == 0);
+         sleep(10);
+      }
+      else
+         creado = 1;
+   } while (creado == 0);
 
+   nombre = getpid();
+   datos.nombre = nombre;
 
-  nombre = getpid();
-  datos.nombre = nombre;
-  
-  // Nombre de un segundo pipe
-  strcpy(datos.secondpipe, "mypipe");
-  // Se crea un segundo pipe para la comunicacion con el server.
-  unlink("mypipe"); 
-  if (mkfifo (datos.secondpipe, fifo_mode) == -1) {
-     perror("Client  mkfifo");
-     exit(1);
-  }
-  // se envia el nombre del pipe al otro proceso. 
-   write(fd, &datos , sizeof(datos));
+   // Nombre de un segundo pipe
+   strcpy(datos.secondpipe, "mypipe");
+   // Se crea un segundo pipe para la comunicacion con el server.
+   unlink("mypipe");
+   if (mkfifo(datos.secondpipe, fifo_mode) == -1)
+   {
+      perror("Client  mkfifo");
+      exit(1);
+   }
+   // se envia el nombre del pipe al otro proceso.
+   write(fd, &datos, sizeof(datos));
 
    // Se abre el segundo pipe
-  creado = 0;
-  do { 
-     if ((fd1 = open(datos.secondpipe, O_RDONLY)) == -1) {
-        perror(" Cliente  Abriendo el segundo pipe. Se volvera a intentar ");
-        sleep(5);
-     } else creado = 1; 
+   creado = 0;
+   do
+   {
+      if ((fd1 = open(datos.secondpipe, O_RDONLY)) == -1)
+      {
+         perror(" Cliente  Abriendo el segundo pipe. Se volvera a intentar ");
+         sleep(5);
+      }
+      else
+         creado = 1;
    } while (creado == 0);
- 
-   // Se lee un mensaje por el segundo pipe. 
+
+   // Se lee un mensaje por el segundo pipe.
 
    read(fd1, mensaje, 10);
    printf("El proceso cliente termina y lee %s \n", mensaje);
- 
-  exit(0);
 
-  
+   exit(0);
 }
